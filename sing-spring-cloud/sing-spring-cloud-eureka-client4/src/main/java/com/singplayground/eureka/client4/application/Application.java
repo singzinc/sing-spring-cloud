ad2d2154e963c97c490527e49d5dd9a681fa02a5
singzinc <sing.zinc.hk@gmail.com>
2018-02-04 18:55:02 +0800

    insert more example

--- sing-spring-cloud/sing-spring-cloud-eureka-client4/src/main/java/com/singplayground/eureka/client4/application/Application.java
+++ sing-spring-cloud/sing-spring-cloud-eureka-client4/src/main/java/com/singplayground/eureka/client4/application/Application.java
@@ -1,21 +1,23 @@
 package com.singplayground.eureka.client4.application;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan("com.singplayground.eureka.client4")
+@EnableAutoConfiguration
 @RestController
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
  
-    @RequestMapping("/info")
-    public String info() {
-        return "the message from client 4";
-    }
+   
 }
